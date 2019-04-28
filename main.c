dab8849161442c8ff422985c4d4fa295871032c4
BrigadeROG1776 <Dyiemboiro@gmail.com>
2019-04-28 02:00:59 +0100

    Update + fix
    Menu de la fonction main

--- main.c
+++ main.c
@@ -5,20 +5,69 @@
 
 int main()
 {
+    float coef;
+    int deg, choix;
 
-    Polynome *poly = initialisation();
-    Polynome *poly2 = initialisation();
+    do
+    {
+        menu();
+        printf("choix : ");
+        scanf("%d", &choix);
+        fflush(stdin);
 
-    insertion(poly, 2, 2);
-    insertion(poly, 4, 1);
-    affichageDec(poly);
+        switch (choix)
+        {
+        case 1:
+            /* code */
+            break;
+        case 2:
+            /* code */
+            break;
+        case 3:
+            /* code */
+            break;
+        case 4:
+            /* code */
+            break;
+        case 5:
+            /* code */
+            break;
+        case 6:
+            /* code */
+            break;
+        case 7:
+            /* code */
+            break;
+        case 8:
+            /* code */
+            break;
+        case 9:
+            /* code */
+            break;
+        case 10:
+            /* code */
+            break;
+        case 11:
+            /* code */
+            break;
+        case 12:
+            /* code */
+            break;
+        case 13:
+            /* code */
+            break;
+        case 14:
+            /* code */
+            break;
+        case 15:
+            printf("FIN DU PROGRAMME\n");
+            break;
 
-    insertion(poly2, 1, 1);
-    insertion(poly2, 5, 10);
-    affichageDec(poly2);
-
-    affichageDec(somme(poly, poly2));
-    affichageDec(soustraction(poly, poly2));
+        default:
+            printf("touche inconnue !\n");
+            break;
+        }
+    } while (choix != 15);
 
     system("pause");
     return 0;
